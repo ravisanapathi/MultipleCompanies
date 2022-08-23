96b6160e16753ba3cbdd7d8124d2e2cebc171e2f
ravisanapathi <ravisanapathi007@gmail.com>
2022-08-23 19:45:27 +0530

    Added Multiple Employees

--- com/bridgelabz/CompanyMain.java
+++ com/bridgelabz/CompanyMain.java
@@ -1,5 +1,13 @@
 package com.bridgelabz;
 
 public class CompanyMain {
+	
+	public static void main(String[] args){
+		Company company = new Company();
+		company.addEmployees();
+		company.addEmployees();
+		company.addEmployees();
+		System.out.println(company.arraylist);
+	}
 
 }
